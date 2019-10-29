80a16b397054aa627856d3176d2f76d5e1f7eeba
daniel-gif-code <2976779737@qq.com>
2019-10-29 08:29:14 +0800

    抽象工厂

--- CreationalPattern/AbstractFactory/AbstractFactory.pro
+++ CreationalPattern/AbstractFactory/AbstractFactory.pro
@@ -17,10 +17,14 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    product.cpp \
+    abstractfactory.cpp
 
 HEADERS += \
-    mainwindow.h
+    mainwindow.h \
+    product.h \
+    abstractfactory.h
 
 FORMS += \
     mainwindow.ui
--- CreationalPattern/AbstractFactory/mainwindow.cpp
+++ CreationalPattern/AbstractFactory/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "AbstractFactory.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    AbstractFactory* cf1 = new ConcreteFactory1();
+    cf1->CreateProductA();
+    cf1->CreateProductB();
+    AbstractFactory* cf2 = new ConcreteFactory2();
+    cf2->CreateProductA();
+    cf2->CreateProductB();
+
 }
 
 MainWindow::~MainWindow()
